27b1b485fbdd0ee76c43f48e8e251e8bf5ba41fe
chaudharyShiv70 <chaudharyshiv7054@gmail.com>
2023-08-06 01:01:16 +0530

    final code
    ecommerce mern stack app

---
+++ server.js
@@ -0,0 +1,56 @@
+// const express = require('express');
+// const colors = require('colors');
+// in es module we use import from as we use in react
+
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoute from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
+import path from "path";
+
+// configure env
+// if .env file does not belongs to root  then we wil
+// crreat object like given bellow and then give path
+// dotenv.config({path:''})
+dotenv.config();
+
+// database config
+connectDB();
+
+// rest object
+const app = express();
+
+//middelwares
+app.use(cors());
+// to enable json
+app.use(express.json());
+// to configure morgan
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+// routes
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/category", categoryRoutes);
+// app.use("api/v1/product", productRoutes);
+app.use("/api/v1/product", productRoutes);
+
+//rest api
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+//port
+const PORT = process.env.PORT || 8080;
+
+//run listen
+app.listen(PORT, () => {
+  console.log(
+    `Server Runninng on ${process.env.DEV_MODE} mode  on port${PORT}`.bgCyan
+      .white
+  );
+});
